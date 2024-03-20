cf241f2efea3dd02a247a770f4815abae2f943aa
kosa12 <kosa03matyas@gmail.com>
2024-03-20 10:31:34 +0200

    asd

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import org.json.simple.parser.JSONParser;
+
+public class Main {
+    public static void main(String[] args) {
+        JSONParser j = new JSONParser();
+    }
+}
