dc5015d8bb02948679f09d4b44938088b1ff94c4
Dacz Krisztian <dacz.krisztian@stud.ubbcluj.ro>
2024-04-17 16:41:25 +0300

    gifplayer :))

--- src/test/java/client/Client.java
+++ src/test/java/client/Client.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import javax.print.attribute.standard.Media;
 import javax.swing.*;
 
 public class Client {
@@ -76,6 +77,11 @@ public class Client {
             }
         }
     }
+
+    public void playGIF(String filePath){
+
+    }
+
 
     public void setOutputTextArea(JTextArea outputTextArea) {
         Client.outputTextArea = outputTextArea;
